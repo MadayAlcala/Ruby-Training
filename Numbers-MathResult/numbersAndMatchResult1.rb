a4ddbba2faedbe50c6a849ed3dd85a9750ba32a6
Maday Alcala <madayalcala@gmail.com>
2019-12-05 14:22:06 -0400

    New file Numbers-MathResult with the second practice added.

---
+++ Numbers-MathResult/numbersAndMatchResult1.rb
@@ -0,0 +1,23 @@
+=begin
+This script contain basic arithmetic operations.
+=end
+puts "This is the proof of ruby addition--> 2 + 1"
+puts "Add:  #{2 + 1}"
+
+puts "This is the proof of substraction--> 2 - 1"
+puts "Subs: #{2-1}"
+
+puts "This is the proof of multiplication--> 2 * 3"
+puts "Mult: #{2 * 3}"
+
+puts "This is the proof of division--> 6 / 2"
+puts "Div: #{6/2}"
+
+puts "This is the proof of exponentiation--> 3 ** 3"
+puts "Exp: #{3**3}"
+
+puts "This is the proof of Modulus Division --> 10 % 3"
+puts "Mod: #{10%3}"
+
+puts "This is the proof of all the operations --> 8+9/2**32*7-10%1"
+puts "All: #{((8+(9/2))**3)-(2*7-10%1)}"
---
+++ Numbers-MathResult/numbersAndMatchResult2.rb
@@ -0,0 +1,13 @@
+=begin
+This script contain basic practice about strings and numbers operations.
+=end
+
+puts "This is an example of how many minuts three hours has:"
+puts "Three hours has: #{3* 60} minuts"
+puts ""
+puts "This is an example of how many hours are 3689 seconds:"
+puts "77777 seconds are #{((77777/60)/60)}"
+puts ""
+puts "The Solar system use to have 9 planets, but now is one less, so now we have 9 - 1 = #{9 - 1}"
+
+#-The values inside #{} are considered like it were outside the string. Eg.: In the numbers case, they are considered just like numbers and not like string.
