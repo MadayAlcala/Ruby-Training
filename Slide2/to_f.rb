66f1469f21bc996e182cc340ccb314bf138a4813
Maday Alcala <madayalcala@gmail.com>
2019-12-05 20:23:29 -0400

    Conversion to integers and to floats numbers added, function practice

---
+++ Slide2/to_f.rb
@@ -0,0 +1,4 @@
+print "Give me a number:"
+number = gets.chomp.to_f
+double = number + number
+puts "The double of the number #{number.to_i} is #{double}"
---
+++ Slide2/to_i.rb
@@ -0,0 +1,4 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+cube = number ** 3 
+puts " The number #{number} raised to 3rd power is #{cube}."
